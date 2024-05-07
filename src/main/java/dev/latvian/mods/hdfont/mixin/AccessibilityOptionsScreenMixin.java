a76905546344b7bc0891208fc11bcb203599ac66
LatvianModder <latvianmodder@gmail.com>
2024-05-07 06:01:41 +0300

    Changed mixin to mixinextras ModifyReturnValue

--- src/main/java/dev/latvian/mods/hdfont/mixin/AccessibilityOptionsScreenMixin.java
+++ src/main/java/dev/latvian/mods/hdfont/mixin/AccessibilityOptionsScreenMixin.java
@@ -1,22 +1,19 @@
 package dev.latvian.mods.hdfont.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.latvian.mods.hdfont.HDFontsClient;
 import net.minecraft.client.gui.screen.option.AccessibilityOptionsScreen;
-import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.SimpleOption;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AccessibilityOptionsScreen.class)
 public class AccessibilityOptionsScreenMixin {
-	@Inject(method = "getOptions", at = @At("RETURN"), cancellable = true)
-	private static void hdfont$getOptions(GameOptions gameOptions, CallbackInfoReturnable<SimpleOption<?>[]> cir) {
-		var options = cir.getReturnValue();
+	@ModifyReturnValue(method = "getOptions", at = @At("RETURN"))
+	private static SimpleOption<?>[] hdfont$getOptions(SimpleOption<?>[] options) {
 		var newOptions = new SimpleOption<?>[options.length + 1];
 		System.arraycopy(options, 0, newOptions, 0, options.length);
 		newOptions[options.length] = HDFontsClient.OPTION;
-		cir.setReturnValue(newOptions);
+		return newOptions;
 	}
 }
